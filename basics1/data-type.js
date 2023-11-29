4ab3048279b9b92ac332fc8a120a60a9d438f5c2
Deepak Yadav <118674072+Deepak-Yadav01@users.noreply.github.com>
2023-11-29 09:41:47 +0000

    learn about data-type

---
+++ basics1/data-type.js
@@ -0,0 +1,19 @@
+//There are mainly two type of data type
+//1. Primitive
+//2. Non primitive
+
+//PRIMITIVE
+
+/* String
+Number
+Boolean
+Null
+Undefined
+Symbol
+BigInt*/
+
+//REFERENCE(NON PRIMITIVE)
+//1. ARRAY -   []
+//2. OBJECT - {}
+//3. FUNCTION -  function(){}
+
