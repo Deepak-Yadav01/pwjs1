485c11d18d1358cbe303518c5548fa4310ff5b9a
Deepak Yadav <118674072+Deepak-Yadav01@users.noreply.github.com>
2023-11-29 06:31:36 +0000

    learn variables and consoles

---
+++ basics1/variables.js
@@ -0,0 +1,9 @@
+//variables - let(keyword we uses) while
+//constant - const(keyword we uses)
+
+const userEmail = "dspyadavdeepak01@gmail.com"
+const passward = "1234"
+
+passward ="1122"
+
+console.log(passward)
